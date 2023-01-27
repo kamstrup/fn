93818ac1c5452d3d1172c859c16cc2a6bfb0ba1c
Mikkel Kamstrup Erlandsen <mikkel@axiom.co>
2023-01-27 09:18:17 +0100

    Add fnmath package with a MakeStats collector

---
+++ math/stats.go
@@ -0,0 +1,25 @@
+package fnmath
+
+import (
+	"github.com/kamstrup/fn/constraints"
+)
+
+// Stats is the result structure used by MakeStats
+type Stats[N constraints.Arithmetic] struct {
+	Sum, Min, Max N
+	Count         int
+}
+
+// MakeStats is a fn.FuncCollect that computes basic numeric properties of a seq of numbers.
+func MakeStats[N constraints.Integer | constraints.Float](s Stats[N], n N) Stats[N] {
+	if s.Count == 0 || n > s.Max {
+		s.Max = n
+	}
+	if s.Count == 0 || n < s.Min {
+		s.Min = n
+	}
+	s.Sum += n
+	s.Count++
+
+	return s
+}
---
+++ math/stats_test.go
@@ -0,0 +1,76 @@
+package fnmath
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/kamstrup/fn"
+)
+
+func TestStats(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  []int
+		expect Stats[int]
+	}{
+		{
+			name:   "empty",
+			input:  nil,
+			expect: Stats[int]{},
+		},
+		{
+			name:  "zero",
+			input: []int{0},
+			expect: Stats[int]{
+				Count: 1,
+			},
+		},
+		{
+			name:  "one",
+			input: []int{1},
+			expect: Stats[int]{
+				Sum:   1,
+				Min:   1,
+				Max:   1,
+				Count: 1,
+			},
+		}, {
+			name:  "minus one",
+			input: []int{-1},
+			expect: Stats[int]{
+				Sum:   -1,
+				Min:   -1,
+				Max:   -1,
+				Count: 1,
+			},
+		},
+		{
+			name:  "range",
+			input: []int{-1, 0, 1, 2},
+			expect: Stats[int]{
+				Sum:   2,
+				Min:   -1,
+				Max:   2,
+				Count: 4,
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			data := fn.ArrayOf(tc.input)
+			opt := fn.Into(Stats[int]{}, MakeStats[int], data)
+			if len(tc.input) == 0 {
+				if opt.Ok() {
+					t.Fatalf("Stats opt should be empty when input is empty")
+				}
+			} else {
+				stats := opt.Must()
+				if !reflect.DeepEqual(stats, tc.expect) {
+					t.Fatalf("stats mismatch\nexpected: %v\ngot:      %v", tc.expect, stats)
+				}
+			}
+		})
+	}
+
+}

e287372e7d9eed503f19abcf92d03a34c96bbc03
Mikkel Kamstrup Erlandsen <mikkel@axiom.co>
2023-01-28 17:52:44 +0100

    fix fx.Keys() copy pasta

--- fx/maps.go
+++ fx/maps.go
@@ -1,10 +1,10 @@
 package fx
 
 // Keys returns a slice with all keys from a map.
-func Keys[K comparable, V any](m map[K]V) []V {
-	keys := make([]V, 0, len(m))
-	for _, v := range m {
-		keys = append(keys, v)
+func Keys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
 	}
 	return keys
 }

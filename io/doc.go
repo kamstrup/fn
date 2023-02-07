8fa0cd4011aa97bf0f97e9fb4f023c8a7ecb93b4
Mikkel Kamstrup Erlandsen <mikkel@axiom.co>
2023-02-07 20:35:23 +0100

    mark seqio as experimental

--- io/doc.go
+++ io/doc.go
@@ -1,3 +1,3 @@
-// Package seqio provides Seq interoperability with
+// Package seqio is an experimental package providing Seq interoperability with
 // standard Go IO interfaces like io.Reader and bufio.Scanner.
 package seqio

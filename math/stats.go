c22887821760f1b8f16ddb342086de2b41e24dd8
Mikkel Kamstrup Erlandsen <mikkel@axiom.co>
2023-02-08 09:47:43 +0100

    MakeStats example in the docs

--- math/stats.go
+++ math/stats.go
@@ -11,6 +11,12 @@ type Stats[N constraints.Arithmetic] struct {
 }
 
 // MakeStats is a seq.FuncCollect that computes basic numeric properties of a seq of numbers.
+//
+// # Example
+//
+//	 data := seq.SliceOfArgs(-1, 0, 1, 2, 3)
+//		optStats := seq.Reduce(MakeStats[int], Stats[int]{}, data)
+//	 // The stats are now {Sum: 5, Min: -1, Max: 3, Count: 5}
 func MakeStats[N constraints.Integer | constraints.Float](s Stats[N], n N) Stats[N] {
 	if s.Count == 0 || n > s.Max {
 		s.Max = n
